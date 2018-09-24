056c2f4f54ed59c844f0d595be4122f8e2e6dbcc
Roman <r.pulgrabja@stud.uni-heidelberg.de>
2018-09-24 13:56:14 +0200

    bla

--- angular-vis/src/app/vis/vis.component.ts
+++ angular-vis/src/app/vis/vis.component.ts
@@ -73,28 +73,47 @@ export class visComponent {
         for (let i in res) {
           inputData.push(res[i]);
         }
-        // for (let i in res){
-        //   let newClusterNode = {
-        //     x: res[i].x,
-        //     y: res[i].y,
-        //     id: -i,
-        //     level: res[i].cid,
-        //     group: 'myGroup'
-        //   };
-        //   inputData.push(newClusterNode);
+        let dataSet = new DataSet();
+
+        //function to determine zoomdepth of graph VERY fast
+        function getMaxZoomLevel() {
+          return inputData.reduce((max, p) => p.level > max ? p.level : max, inputData[0].level);
+        };
+        let maxLevel = getMaxZoomLevel();
+        for (let i in res) {
+          console.log(res[i].level);
+          if (res[i].level < maxLevel) {
+            let newClusterNode = {
+              x: res[i].x,
+              y: res[i].y,
+              id: 'ring' + res[i].id,
+              parent: 'ring' + (res[i].parent),
+              level: res[i].level,
+              group: 'myGroup'
+            };
+            inputData.push(newClusterNode);
+          }
+        }
+
+        inputData.forEach((elem, index, array) => {
+          dataSet.add({id: elem.id, x: elem.x, y: elem.y, parent: elem.parent, fixed: true, physics: false, shape: elem.shape, image: elem.img, label: elem.label, group: elem.group, level: elem.level});
+        });
         // }
         // let masterCluster = {
         //   x: res[0].x,
         //   y: res[0].y,
-        //   id: -99999,
-        //   //cid: res[i].cid,
+        //   id: 99,
+        //   level: maxLevel,
+        //   //parent: 0,
         //   group: 'xxx'
         // };
         // inputData.push(masterCluster);
 
         //console.log(inputData)
+        //let dataSet = new this.network.DataSet();
+
         var data = {
-          nodes: this.getNodeData(inputData),
+          nodes: dataSet,//this.getNodeData(inputData), => making node-data dynamical
           edges: this.getEdgeData(inputData)
         };
 
@@ -104,69 +123,78 @@ export class visComponent {
               y: true,
               x: true
             }
-          }
-          // ,
-          // groups: {
-          //   myGroup: {
-          //     shape: 'dot',
-          //     size: 250,
-          //     color:{background:'transparent', border: "yellow"}, borderWidth:8},
-          //   xxx: {
-          //     shape: 'dot',
-          //     size: 750,
-          //     color:{background:'transparent', border: "red"}, borderWidth:8
-          //   }
-          // }
+          },
+          groups: {
+            myGroup: {
+              shape: 'dot',
+              size: 250,
+              color:{background:'transparent', border: "#f1eec1"},
+              borderWidth: 10
+            },
+            xxx: {
+              shape: 'dot',
+              size: 760,
+              color:{background:'transparent', border: "#dbd7d2"}, borderWidth: 10
+            }
+          },
+          width: (window.innerWidth) + 'px',
+          height: (window.innerHeight) + 'px'
         };
 
         this.network = new Network(this.networkDiv.nativeElement, data, options);
+        // let imageb = new Image();
+        // imageb.src = '../../assets/images/HINTERGRUND.png';
+        // this.network.on("beforeDrawing", function(ctx) {
+        //   ctx.drawImage(imageb, -700, -600);
+        // });
         // set the first initial zoom level
         this.network.once('initRedraw', () => {
           if (this.lastClusterZoomLevel === 0) {
             this.lastClusterZoomLevel = this.network.getScale();
           }
         });
-        //function to determine zoomdepth of graph VERY fast
-        function getMaxZoomLevel() {
-          return inputData.reduce((max, p) => p.level > max ? p.level : max, inputData[0].level);
-        };
-        var maxLevel = getMaxZoomLevel();
-        console.log(maxLevel);
+
         // for (let level = maxLevel; level > 0; level--) {
         //   this.clusterByCid(level);
         // }
         console.log(this.network.body);
-        let zoomlevel = getMaxZoomLevel(); //setting intial value for zoomlevel at 0
+        let zoomlevel = maxLevel;//getMaxZoomLevel(); //setting intial value for zoomlevel at 0
 
         // we use the zoom event for our clustering
         this.network.on('zoom', (params) => {
-          if (params.direction === '-' && zoomlevel >= 0) {
-            if (params.scale < this.lastClusterZoomLevel * this.clusterFactor) {
-              //console.log(params.scale);
+          if (params.direction === '-' && zoomlevel > 0) {
+            //if (params.scale < this.lastClusterZoomLevel * this.clusterFactor) {
               this.clusterByCid(zoomlevel);
+              if (this.network.body.nodes['master'] !== undefined) {
+                dataSet.remove('master');
+              }
               if (zoomlevel !== 0) {
                 zoomlevel -= 1;
               }
-              if (this.lastClusterZoomLevel > 0.7 || this.lastClusterZoomLevel < 0.5) {
-                this.lastClusterZoomLevel = 0.75;
-              }
-              //console.log(zoomlevel);
-              this.lastClusterZoomLevel = params.scale;
-            }
+              // if (this.lastClusterZoomLevel > 0.7 || this.lastClusterZoomLevel < 0.5) {
+              //   this.lastClusterZoomLevel = 0.75;
+              // }
+              // //console.log(zoomlevel);
+              // this.lastClusterZoomLevel = params.scale;
+            //}
             //only trigger open cluster if there is a cluster to open
           } else if (params.direction === '+' && zoomlevel < maxLevel) {  //&& this.network.body.nodes['-1'] !== undefined
-            console.log('scale: ' + params.scale);
-            console.log('zoomlevel: ' + (this.lastClusterZoomLevel * this.clusterFactor));
-            if (params.scale  >= this.lastClusterZoomLevel * this.clusterFactor) {
+            // console.log('scale: ' + params.scale);
+            // console.log('zoomlevel: ' + (this.lastClusterZoomLevel * this.clusterFactor));
+            //if (params.scale  >= this.lastClusterZoomLevel * this.clusterFactor) {
               for (let cluster in this.network.body.nodes) {
                   if (this.network.body.nodes[cluster].isCluster === true && this.network.body.nodes[cluster].options.level === zoomlevel) {
+
                     this.network.openCluster(this.network.body.nodes[cluster].id);
+                    if (this.network.body.nodes['master'] === undefined) {
+                      dataSet.add({id: 'master', x: 0, y: 0, group: 'xxx'});
+                    }
                   }
                 }
-              }
+             // }
               zoomlevel += 1;
               //console.log(zoomlevel);
-              this.lastClusterZoomLevel = params.scale;
+              //this.lastClusterZoomLevel = params.scale;
 
           }
         });
@@ -180,24 +208,24 @@ export class visComponent {
         this.network.on('click', (params) => {
           if (params.nodes.length > 0) {
             var clickedId = params.nodes[0];
-            var inputData = this.test;//JSON.parse(inputValue) => res to show sidenav again   need to find correct type cast
-            var clickedNode = inputData.filter(
-              function(inputData) { return inputData['id'] === clickedId;
-              });
-            this.clickedNodeName = clickedNode[0].label;
-            this.clickedNodeImg = clickedNode[0].img;
-
+            console.log(clickedId);
+            console.log(this.network.body.nodes[clickedId].options.level);
+            // var inputData = this.test;//JSON.parse(inputValue) => res to show sidenav again   need to find correct type cast
+            // var clickedNode = inputData.filter(
+            //   function(inputData) { return inputData['id'] === clickedId;
+            //   });
+            this.clickedNodeName = this.network.body.nodes[clickedId].options.label;//clickedNode[0].label;
+            this.clickedNodeImg = this.network.body.nodes[clickedId].options.image;//clickedNode[0].img;
+            //
             this.sidenavInfo.toggle();
           }
         });
-
-        this.resizeExportArea();
       },
       err => {
         console.error('Error while reading data from server: ');
         console.error(err);
       });
-
+    //this.resizeExportArea();
   }
 
   getNodeData(data) {
